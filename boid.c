b763f8ce43526e286448bf0adefc102954f784e5
kickass <virgile.dauge@telecomnancy.net>
2014-04-21 01:02:15 +0200

    ajout Boid et tostring_vecteur

---
+++ boid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "boid.h"
+#include "vecteur.h"
+
+boid_t *new_boid(vecteur_t *v){
+	boid_t *b = malloc(sizeof(v));
+	return b;
+}
+void set_boid_pos(boid_t *b, vecteur_t *v){
+	b->v = v;
+}
+vecteur_t *get_boid_pos(boid_t *b){
+	return b->v;
+}
+void print_boid(boid_t *b){
+	printf("<&°%st><l\n",toString_vecteur(b->v));	
+}
---
+++ boid.h
@@ -0,0 +1,13 @@
+#ifndef BOID_H
+#define BOID_H
+
+typedef struct boid{
+	int id;
+	vecteur_t *v;
+} boid_t;
+
+boid_t *new_boid(vecteur_t *v);
+void set_boid_pos(boid_t *b, vecteur_t *v);
+vecteur_t *get_boid_pos(boid_t *b);
+void print_boid(boid_t *b);
+#endif
