5f18cb7a542ac86f413e569871d9ec1247268a99
kickass <virgile.dauge@telecomnancy.net>
2014-04-22 15:55:42 +0200

    ajout base SDL (en cours)

--- main.c
+++ main.c
@@ -3,17 +3,19 @@
 #include "main.h"
 #include "modele.h"
 #include "vecteur.h"
+#include <SDL2/SDL.h>
+
 
 int main(int argc, char * argv){
-	/*
-	int a=1;
-	printf("%d\n", a);
-	calcul_deplacements(&a);
-	printf("%d\n", a);
-	*/
+	init();
 }
 int init(){
-
+	/* Initialisation simple */
+	if (SDL_Init(SDL_INIT_ALL) != 0 ){
+		fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+	return -1;
+	}
+	SDL_Window* pWindow = = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 }
 
 
