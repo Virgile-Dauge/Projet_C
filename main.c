08b3193de7db3866f60905825f57b3da13ea6543
kickass <virgile.dauge@telecomnancy.net>
2014-04-24 11:43:55 +0200

    debut sdl2

--- main.c
+++ main.c
@@ -7,15 +7,37 @@
 
 
 int main(int argc, char * argv){
-	init();
+	SDL_Window* pWindow = NULL;
+	init(pWindow);
+	SDL_Event event;
+	int quit =0;
+	int fullscreen =0;
+	while(SDL_PollEvent(&event)){
+		switch(event.type)
+		{
+			//case SDL_QUIT: quit=1; break;
+			case SDL_KEYUP:
+				if(event.key.keysym.sym == SDLK_f){
+					if(fullscreen){
+						fullscreen=0;
+						SDL_SetWindowFullscreen(pWindow,0);
+					}else{
+						fullscreen=1;
+						SDL_SetWindowFullscreen(pWindow,SDL_WINDOW_FULLSCREEN);
+					}
+				}
+				break;	
+		}
+	}
+
 }
-int init(){
+int init(SDL_Window* fenetre){
 	/* Initialisation simple */
-	if (SDL_Init(SDL_INIT_ALL) != 0 ){
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0 ){
 		fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
 	return -1;
 	}
-	SDL_Window* pWindow = = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	fenetre = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN);
 }
 
 
